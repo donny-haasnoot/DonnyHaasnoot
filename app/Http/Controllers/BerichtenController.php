18e03db743f91546dd145315e7ad77f01b8a8140
Donny Haasnoot <81227730+donny-haasnoot@users.noreply.github.com>
2022-03-07 11:32:38 +0100

    ja

---
+++ app/Http/Controllers/BerichtenController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Berichten;
+use Illuminate\Http\Request;
+
+class BerichtenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Berichten  $berichten
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Berichten $berichten)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Berichten  $berichten
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Berichten $berichten)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Berichten  $berichten
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Berichten $berichten)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Berichten  $berichten
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Berichten $berichten)
+    {
+        //
+    }
+}
